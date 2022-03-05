031fa93fc81b424ab98497c010afb01b832254b6
Muhammad A Faishal <maafaishal@gmail.com>
2022-03-06 01:26:19 +0700

    fix: viewport

--- app/root.tsx
+++ app/root.tsx
@@ -44,6 +44,8 @@ export function CatchBoundary() {
   return (
     <html>
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <title>Halaman tidak ditemukan | Alumni Sakura</title>
         <Meta />
         <Links />
